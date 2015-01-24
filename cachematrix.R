48d716b588009f4101cfa99eaad3eaf27ba52718
DataAnalysis <rajeev@Rajeev-Srivastavas-iMac.local>
2015-01-24 21:28:48 +0530

    Programming assignment
    R Programming second programming assignment.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,58 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+
+## Function creates a special "Matrix", 
+## which is really a list containing a function to
+## set the value of the Matrix
+## get the value of the Matrix
+## set the value of the Inverse of Matrix
+## get the value of the Inverse of Matrix
 
 makeCacheMatrix <- function(x = matrix()) {
 
+  ## Set the value of the matrix
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  
+  ## Get the value of the matrix
+  get <- function() x
+  
+  ## Set and set the inverse of the matrix
+  setmatrix <- function(solve) m <<- solve
+  getmatrix <- function() m
+  list(set = set, get = get,
+       setmatrix = setmatrix,
+       getmatrix = getmatrix)
 }
 
-
-## Write a short comment describing this function
+# Retruns inverse of the matrix. If inverse already calculated then retrun cached inverse of matrix.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  
+  ## get the inverse of the matrix    
+  m <- x$getmatrix()
+  
+  ## check If inverse already calculated
+  if(!is.null(m)) {
+    message("Getting cached inverse of matrix.")
+    return(m)
+  }
+  ## if not get the inverse of the matrix   
+  data <- x$get()
+  ## Checking for error while computing inverse of matrix.
+  m <- tryCatch(
+       {
+           solve(data, ...) 
+       }, 
+       error=function(e)
+      {
+          return("Error while computing inverse of matrix using solve(). Matrix must be invertable using solve().")
+      })
+  x$setmatrix(m)
+  m
 }
